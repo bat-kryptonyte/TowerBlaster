39d3d7a1d3fa6907ea7538d2f1d0a5a26987ada2
Jeffrey Deng <jeffreytheflash123@gmail.com>
2019-11-17 11:45:33 -0800

    Final

---
+++ Deck.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Deck {
+    private ArrayList<Integer> tower;
+
+    public Deck(int initSize) {
+        tower = new ArrayList<Integer>();
+        for (int i = 1; i <= initSize; i++) {
+            tower.add(i);
+        }
+
+    }
+
+    public Deck(ArrayList<Integer> t) {
+        tower = t;
+    }
+
+    public void shuffle() {
+        for (int i = tower.size() - 1; i >= 0; i--) {
+            int rand = (int) (Math.random() * tower.size());
+            int temp = tower.get(rand);
+            tower.set(rand, tower.get(i));
+            tower.set(i, temp);
+        }
+
+    }
+
+    public int[] newTower(int initHeight) {
+        int[] newT = new int[initHeight];
+        for (int i = 0; i < newT.length; i++) {
+            newT[i] = tower.remove(0);
+        }
+        return newT;
+    }
+
+    public void replaceA(Deck x) {
+        x.getTower().add(0, tower.remove(0));
+    }
+
+    public String showCard() {
+        String result = Integer.toString(tower.get(0));
+        return result;
+    }
+
+    public void replace(int index, int[] playerT, Deck x) {
+        int temp = playerT[index];
+        playerT[index] = tower.remove(0);
+        x.getTower().add(0, temp);
+    }
+
+    public ArrayList<Integer> getTower() {
+        return tower;
+    }
+
+    public String toString() {
+        String sum = "";
+        for (int i = 0; i < tower.size(); i++) {
+            sum += Integer.toString(tower.get(i)) + "\n";
+        }
+        return sum;
+    }
+}
---
+++ Driver.java
@@ -0,0 +1,185 @@
+import java.util.*;
+
+public class Driver {
+    public static final int USER_TOWER_HEIGHT = 10;
+    public static final int DECK_LENGTH = 50;
+
+    public static void main(String[] args) {
+
+        while (true) {
+
+            Scanner kb = new Scanner(System.in);
+            int tl = DECK_LENGTH;
+            int score = 0;
+            Deck tower = new Deck(tl);
+            tower.shuffle();
+            int[] pt = tower.newTower(USER_TOWER_HEIGHT);
+            int[] vikings = tower.newTower(USER_TOWER_HEIGHT);
+            Deck discardPile = new Deck(0);
+            tower.replaceA(discardPile);
+            System.out.println(displayTower(pt));
+            System.out.println(displayTower(vikings) + "<---- Your scout had uncovered an enemy raven");
+
+            while (!isWin(pt) || !isWin(vikings)) {
+                // User steps
+
+                System.out.println(
+                        "The card you can choose is: " + discardPile.showCard() + " or you can choose UNKNOWN");
+                System.out.println("Please select your choice: ");
+                String userInput = kb.next();
+                // error check loop
+                while (!userInput.equalsIgnoreCase(discardPile.showCard()) && !userInput.equalsIgnoreCase("UNKNOWN")) {
+                    System.out.println("Please enter a valid answer");
+                    userInput = kb.next();
+                }
+                if (userInput.equalsIgnoreCase(discardPile.showCard())) {
+                    System.out.println("Please enter the block you wish to switch: ");
+                    int userChoice = kb.nextInt();
+                    // error check loop
+                    while (getIndex(userChoice, pt) == -1) {
+                        System.out.println("Please enter a valid choice");
+                        userChoice = kb.nextInt();
+                    }
+                    int ind = getIndex(userChoice, pt);
+                    discardPile.replace(ind, pt, discardPile);
+                    score += getScore(pt, ind);
+                    System.out.println(displayTower(pt));
+                    System.out.println("Your score is currently at : " + score);
+                } else if (userInput.equalsIgnoreCase("UNKNOWN")) {
+                    System.out.println("The card you can choose is: " + tower.showCard());
+                    System.out.println("Please choose YES, or DISCARD(YOUR TURN WILL BE SKIPPED: ");
+                    String uI = kb.next();
+                    // error check loop
+                    while (!uI.equalsIgnoreCase("YES") && !uI.equalsIgnoreCase("DISCARD")) {
+                        System.out.println("Please enter a valid choice");
+                        uI = kb.next();
+                    }
+                    if (uI.equalsIgnoreCase("YES")) {
+                        System.out.println("Please enter the block you wish to switch: ");
+                        int userChoice = kb.nextInt();
+                        // error check loop
+                        while (getIndex(userChoice, pt) == -1) {
+                            System.out.println("Please enter a valid choice");
+                            userChoice = kb.nextInt();
+                        }
+                        int ind = getIndex(userChoice, pt);
+                        tower.replace(ind, pt, discardPile);
+                        score += getScore(pt, ind);
+                        System.out.println(displayTower(pt));
+                        System.out.println("Your score is currently at : " + score);
+                    } else if (uI.equalsIgnoreCase("DISCARD")) {
+                        tower.replaceA(discardPile);
+                        System.out.println(displayTower(pt));
+                    }
+                }
+
+                // Viking steps
+                boolean step1 = Math.random() < 0.5;
+                boolean step2 = Math.random() < 0.5;
+                int randV = (int) (Math.random() * USER_TOWER_HEIGHT);
+                int randV2 = (int) (Math.random() * USER_TOWER_HEIGHT);
+                if (step1) {
+                    discardPile.replace(randV, vikings, discardPile);
+                } else {
+                    if (step2) {
+                        tower.replace(randV2, vikings, discardPile);
+                    } else {
+                        tower.replaceA(discardPile);
+                    }
+                }
+                if (isWin(pt)) {
+                    break;
+                } else if (isWin(vikings)) {
+                    break;
+                }
+
+            }
+            if (isWin(pt)) {
+                System.out.println("Congratulations! You have won!");
+                System.out.println("FINAL SCORE: " + score);
+                System.out.println("Would you like to play again(YES or NO): ");
+                String input = kb.next();
+                if (input.equalsIgnoreCase("YES")) {
+                    continue;
+                } else {
+                    kb.close();
+                    break;
+                }
+            } else if (isWin(vikings)) {
+                System.out.println("You have lost! Good Luck next time!");
+                System.out.println("FINAL SCORE: " + score);
+                System.out.println("Would you like to play again(YES or NO): ");
+                String input = kb.next();
+                if (input.equalsIgnoreCase("YES")) {
+                    continue;
+                } else {
+                    kb.close();
+                    break;
+                }
+            }
+
+        }
+
+    }
+
+    public static int getScore(int[] playerTower, int userIndex) {
+        ArrayList<Integer> tempPile = new ArrayList<Integer>();
+        int count = 0;
+        int arrInd = 0;
+        for (int i = 1; i < playerTower.length; i++) {
+            if (playerTower[i] - playerTower[i - 1] == 1) {
+                tempPile.add(i - 1);
+                if (i - 1 == userIndex) {
+                    arrInd = tempPile.get(tempPile.size() - 1);
+                }
+            }
+        }
+        for (int i = 0; i < tempPile.size(); i++) {
+            if (tempPile.get(arrInd) - tempPile.get(i) == arrInd - i) {
+                count++;
+            }
+        }
+        return (int) (Math.pow(count, 2) * playerTower[userIndex]);
+    }
+
+    public static int getIndex(int block, int[] us) {
+        int index = 0;
+        for (int i = 0; i < us.length; i++) {
+            if (us[i] == block) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static String displayTower(int[] pt) {
+        String result = "";
+        for (int i = 0; i < pt.length; i++) {
+            result += pt[i] + " ";
+        }
+        return "This is your deck: " + result;
+
+    }
+
+    public static boolean isWin(int[] playerTower) {
+        boolean value = true;
+        int[] pt = new int[playerTower.length];
+        for (int i = 0; i < pt.length; i++) {
+            pt[i] = playerTower[i];
+        }
+        for (int i = 1; i < pt.length; i++) {
+            if (pt[i] < pt[i - 1]) {
+                int temp = pt[i];
+                pt[i] = pt[i - 1];
+                pt[i - 1] = temp;
+            }
+        }
+        for (int i = 0; i < playerTower.length; i++) {
+            if (pt[i] != playerTower[i]) {
+                return false;
+            }
+        }
+        return value;
+    }
+
+}
